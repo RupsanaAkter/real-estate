6b7be4d234604b4a0496786b62217a4486c1f049
R7558 <rupsanaworld1004@gmail.co>
2023-06-07 04:13:06 +0600

    added blog

--- src/pages/Blog/Blog/Blog.js
+++ src/pages/Blog/Blog/Blog.js
@@ -1,9 +1,17 @@
 import React from 'react';
-
+import Back from '../../Shared/Back/Back';
+import img from '../../../assets/images/about (1).jpg'
+import RecentCard from '../../Home/Recent/RecentCard';
+import '../../Home/Recent/Recent.css'
 const Blog = () => {
     return (
         <div>
-            <h1>This is Blog</h1>
+             <section className='blog-out mb'>
+        <Back name='Blog' title='Blog Grid - Our Blogs' cover={img} />
+        <div className='container recent'>
+          <RecentCard />
+        </div>
+      </section>
         </div>
     );
 };
